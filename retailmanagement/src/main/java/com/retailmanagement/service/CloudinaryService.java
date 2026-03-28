f5ace8009a4ee3fb238590be74cbeb6ce3807d78
Đào Anh Đức <daod1068@gmail.com>
2026-03-28 11:41:43 +0700

    fix many thing as img

---
+++ retailmanagement/src/main/java/com/retailmanagement/service/CloudinaryService.java
@@ -0,0 +1,30 @@
+package com.retailmanagement.service;
+
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(
+                file.getBytes(),
+                ObjectUtils.asMap("folder", "tech-store")
+        );
+        return uploadResult.get("secure_url").toString();
+    }
+
+    public void deleteFile(String publicId) throws IOException {
+        cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+    }
+}
